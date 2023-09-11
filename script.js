786f23853656b9616a6e0adff051c65c58c42701
Mihael <139009336+MihaelYaryomenko@users.noreply.github.com>
2023-09-11 15:57:28 +0700

    Object generator fin

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+const data = []
+
 const dialog = document.querySelector("#book-form")
 
-const data = []
+const submitBtn = document.querySelector("#submit-btn")
 
 const modalOpen = () => {
   dialog.showModal()
@@ -10,13 +12,26 @@ const modalClose = () => {
   dialog.close()
 }
 
-const submitButton = () => {
-  const author = document.querySelector("#author")
-  const title = document.querySelector("#title")
-  const numOfPages = document.querySelector("#num-pages")
-  const isRead = document.querySelector("#isRead")
-  console.log("Hello World!")
+class booksConstructor {
+  constructor(author, title, pages, isRead) {
+    this.author = author
+    this.title = title
+    this.pages = pages
+    this.isRead = isRead
+  }
 }
+
+const submitButton = (event) => {
+  const author = document.querySelector("#author").value
+  const title = document.querySelector("#title").value
+  const numOfPages = document.querySelector("#num-pages").value
+  const isRead = document.querySelector("#isRead").value
+  const newBook = new booksConstructor(author, title, numOfPages, isRead)
+  data.push(newBook)
+  event.preventDefault()
+}
+
+submitBtn.addEventListener("click", submitButton)
 
 dialog.addEventListener("click", e => {
   const dialogDimensions = dialog.getBoundingClientRect()
@@ -29,3 +44,7 @@ dialog.addEventListener("click", e => {
     dialog.close()
   }
 })
+
+const booksGenerator = () => {
+  
+}
